69105a9282540fb91de7bd7a08297916dc5df2be
usuario <usuario@10.22.143.56>
2016-09-23 09:07:58 +0100

    Primera version de Kata1

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,13 @@
+
+package kata1;
+
+import java.util.Date;
+
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        Person person = new Person ("Yo", new Date(96,11,27));
+        System.out.println(person.getName()+ "tengo" + person.getAge() + "años";
+    }
+}
